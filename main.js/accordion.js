78c74d7ed97d00bd414c8b44a26fd9822f0f1550
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2022-09-08 20:45:44 +0300

    ...

---
+++ main.js/accordion.js
@@ -0,0 +1,7 @@
+var acc = document.querySelector(".btn--servicess");
+var panel = document.querySelector(".panel");
+
+acc.onclick = function () {
+    acc.classList.toggle("active");
+    panel.classList.toggle("active");
+};
