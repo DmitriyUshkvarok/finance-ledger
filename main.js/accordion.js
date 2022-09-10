15d368513b84c59aa95636546e932db796a03e28
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2022-09-10 16:52:41 +0300

    fix

--- main.js/accordion.js
+++ main.js/accordion.js
@@ -2,6 +2,6 @@ var acc = document.querySelector(".btn--servicess");
 var panel = document.querySelector(".panel");
 
 acc.onclick = function () {
-    acc.classList.toggle("active");
-    panel.classList.toggle("active");
+  acc.classList.toggle("active");
+  panel.classList.toggle("active");
 };
